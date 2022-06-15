4ce096b70abc9ab0c5a1b55dfa8cfa6a2f85c030
Ben Smith <ben@bensresearch.com>
2022-06-15 16:47:53 -0400

    First untested version

---
+++ ProjectAssessment/Solver.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+from multiprocessing import Pool
+from progress.bar import Bar
+
+def itemPb(q, s, k, b):
+	return (1 - q - s)^(np.floor(k)) * (q + s + (q + s - 1) * np.ceil(-k/b))
+
+def opFunction(x, data):
+	return -1.0 * (np.array([ itemPb(x[item[1]], x[item[2]], item[0], item[3]) for item in data ]).prod())
+
+def solve(dataset):
+	studentCode, uniqueStudents = pd.factorize(dataset['student'])
+	questionCode, uniqueQuestion = pd.factorize(dataset['rubric'])
+	questionCode = questionCode + uniqueStudents.size
+	map = np.concatenate((uniqueStudents, uniqueQuestion), axis=None).tolist()
+	data = list(zip(dataset['k'], studentCode, questionCode, dataset['bound']))
+	minValue = minimize(opFunction, [0]*len(map), args=(data, ), method='Powell')
+	if (minValue.success):
+		fullResults = list(zip(map, minValue.x))
+		studentResults = fullResults[:uniqueStudents.size]
+		questionResults = fullResults[uniqueStudents.size:]
+		return {
+			'student': pd.DataFrame(studentResults, columns=['Variable', 'Value']),
+			'rubric': pd.DataFrame(questionResults, columns=['Variable', 'Value']),
+		}
+	else:
+		return None
+
+def bootstrapRow (dataset, rubric=False):
+	key = 'rubric' if rubric else 'student'
+	ids = dataset[key].unique()
+	randomGroupIds = np.random.choice(ids, size=len(ids), replace=True)
+	l = []
+	for c, i in enumerate(randomGroupIds):
+		rows = dataset[dataset[key]==i]
+		rows[key] = c
+		l.append(rows)
+	resultData = pd.concat(l, ignore_index=True)
+	return solve(resultData)
+
+def CallRow(row):
+	key = 'student' if row['rubric'] else 'rubric'
+	r = bootstrapRow(row['dataset'], row['rubric'])
+	if r is not None:
+		return r[key]
+	return None
+
+def bootstrap(dataset, n, rubric=False):
+	l = []
+	rows = [
+		{
+			'dataset': dataset,
+			'rubric': rubric
+		}
+	]*n
+	bar = Bar('Processing', max=n)
+	p = Pool()
+	nones = []
+	for _, result in enumerate(p.imap_unordered(CallRow, rows)):
+		if result is not None:
+			l.append(result)
+		else:
+			nones.append(1)
+		bar.next()
+	p.close()
+	p.join()
+	bar.finish()
+	return {
+		'results': pd.concat(l, ignore_index=True),
+		'nones': len(nones)
+	}
+
+def getResults(dataset,c=0.025, rubric=False, n=10000):
+	# dataset
+	# [
+	# 	[k, student, rubric, bound],
+	# ]
+	dataset.dropna()
+	estimates = solve(dataset)
+	if estimates is not None:
+		r = bootstrap(dataset, n, rubric)
+		l = []
+		for var in r['Variable'].unique():
+			df = r[r['Variable'] == var]['Value']
+			ci = (df.quantile(q=c), df.quantile(q=(1-c)))
+			l.append({
+				'Variable': var,
+				'Confidence Interval': ci,
+			})
+		return (estimates['rubric'], estimates['student'], pd.DataFrame(l), r['nones'])
+	return None
