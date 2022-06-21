6e5e8d5456d89fe85f573b063b5b80b2cd67b8d8
Ben Smith <ben@bensresearch.com>
2022-06-21 05:10:03 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ProjectAssessment',
-    version='0.0.1',
+    version='0.0.5',
     packages=['ProjectAssessment',],
     license='MIT',
     install_requires=[
@@ -22,6 +22,6 @@ setup(
     ],
     keywords = ['Assessment', 'Projects', 'Statistics', 'Education', 'Bootstrap'],
     url = 'https://github.com/tazzben/project-based-assessment',
-    download_url = 'https://github.com/tazzben/project-based-assessment/archive/v0.0.4.tar.gz',  
+    download_url = 'https://github.com/tazzben/project-based-assessment/archive/v0.0.5.tar.gz',  
     description = 'Package to compute the Project-Based Assessment estimates of student and rubric proficiency.',
 )
