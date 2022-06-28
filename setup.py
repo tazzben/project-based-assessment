73c47e1b384b26c50f62076288fabe36595b52c7
Ben Smith <ben@bensresearch.com>
2022-06-28 08:01:17 -0500

    Decrease version numbers

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 setup(
     name='ProjectAssessment',
-    version='0.0.11',
+    version='0.0.12',
     packages=['ProjectAssessment',],
-    python_requires='>3.9.0',
+    python_requires='>3.7.0',
     license='MIT',
     install_requires=[
         'numpy>=1.21.0',
-        'pandas>=1.4.0',
+        'pandas>=1.3.0',
         'progress>=1.5',
-        'scipy>=1.7.0',
+        'scipy>=1.4.0',
     ],
     author='Ben Smith',
     author_email='bosmith@unomaha.edu',
@@ -19,9 +19,11 @@ setup(
     'Intended Audience :: Science/Research', 
     'License :: OSI Approved :: MIT License',  
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.7',
     ],
     keywords = ['Assessment', 'Projects', 'Statistics', 'Education', 'Bootstrap'],
     url = 'https://github.com/tazzben/project-based-assessment',
-    download_url = 'https://github.com/tazzben/project-based-assessment/archive/v0.0.11.tar.gz',  
+    download_url = 'https://github.com/tazzben/project-based-assessment/archive/v0.0.12.tar.gz',  
     description = 'Package to compute the Project-Based Assessment estimates of student and rubric proficiency.',
 )
