db8f00186b7e91ff1c20b4af23e67aa6a9c3a3bc
Ben Smith <ben@bensresearch.com>
2022-07-04 10:50:33 -0500

    Update Marginal.py

--- ProjectAssessment/Marginal.py
+++ ProjectAssessment/Marginal.py
@@ -4,8 +4,8 @@ from scipy.special import expit
 def probability(q, s, linear = False):
     return q + s if linear else expit(q + s)
 
-def logisticD(q, s, question = False):
-    return q * np.exp(q+s) / (1 + np.exp(q+s))**2 if question else s * np.exp(q+s) / (1 + np.exp(q+s))
+def logisticD(q, s, question = False):    
+    return q * expit(q + s)*(1-expit(q + s)) if question else s * expit(q + s)*(1 - expit(q + s))
 
 def linearD(q, s, question = False):
     return q if question else s
@@ -14,6 +14,8 @@ def probabilityDerivative(q, s, question = False, linear = False):
     return linearD(q, s, question) if linear else logisticD(q, s, question)
 
 def dItemPb(q, s, k, b, question = False, linear = False):
+    if k == 0:
+        return probabilityDerivative(q, s, question, linear)
     return probabilityDerivative(q, s, question, linear)*(-1*(1-probability(q, s, linear))**(np.floor(k)-1))*(-1*(np.floor(k)+1)*np.floor(k/b)*(probability(q, s, linear)-1)+(np.floor(k)+1)*probability(q, s, linear)-1)
 
 def calculateMarginal(position, data, estX, studentSize, linear = False):
