a85d3dd06be6fa829ae167c85204671716cc6d4f
Ben Smith <ben@bensresearch.com>
2022-07-04 14:57:41 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,8 +3,8 @@ import ProjectAssessment as pa
 
 def main():
     print("Dataset One with Linear Model")
-    df = pd.read_csv('data.csv')
-    pa.DisplayResults(df, linear=True)
+    df = pd.read_csv('data2.csv')
+    pa.SaveResults(df, linear=True)
 
 if __name__ == '__main__':
     main()
